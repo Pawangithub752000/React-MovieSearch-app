fffc123786cebb14ea6b766b069afd161cb9d1b8
Pawan Kumar <pawanbtechcse752000@gmail.com>
2022-12-27 19:07:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
+const Container = styled.div`
+display: flex;
+fiex-direction: column;
+`;
 
+const Header = styled.div`
+display: flex;
+flex-direction: row;
+background-color: black;
+color: white;
+padding: 10px;
+font-size: 25px;
+font-weight: bold;
+box-shadow: 0 3px 6px 0 #555;
+
+`;
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header>
+     React Movie App
+        </Header> Pawan prajapati
+    </Container>
   );
 }
 
